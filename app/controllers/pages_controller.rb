dd1f11b25de2c8434b7e8b5ebea8f06eb43e3689
Swapna Brungi <swapna.brungi@gmail.com>
2017-04-10 21:21:12 -0400

    added secret page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   before_action :set_kitten_url, only: [:kitten, :kittens]
 
+  def contest
+  flash[:notice] = "Sorry, the contest has ended"
+  redirect_to "/welcome"
+  end
+
   def welcome
     @header = "This is the welcome page"
   end
@@ -17,4 +22,16 @@ class PagesController < ApplicationController
   @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
   end
 
-end
+  def contest
+    flash[:notice] = "sorry, the contest has ended"
+    redirect_to "/welcome"
+
+  end
+  def secrets
+    secretcode = params[:magic_word]
+    if secretcode == "butterflee"
+      redirect_to "/"
+    end
+  end
+
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
   get '/' => 'pages#welcome'
+  get '/welcome' => 'pages#welcome'
   get '/about' => 'pages#about'
   get '/kitten/:size' => 'pages#kitten'
+  get '/contest' => 'pages#contest'
+  get '/secrets/:magic_word' => 'pages#secrets'
+
 
   # def welcome
   #   @header = "This is the Welcome Page"

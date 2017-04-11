b4085a0f6780f6a9f8afc17844d8d7758321714a
Swapna Brungi <swapna.brungi@gmail.com>
2017-04-10 20:46:28 -0400

    kitten pic lift off

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,11 @@ class PagesController < ApplicationController
     @header = "This is the welcome page"
   end
 
+  def kitten
+  requested_size = params[:size]
+  @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+end
+  def kittens
+  end
+
 end
